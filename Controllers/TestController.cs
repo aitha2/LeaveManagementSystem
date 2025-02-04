7eb9e019a9b1840c24ac029451edbb2204e068c4
sakaitha <sakaitha@USHYDSAKAITHA7>
2025-02-04 22:49:01 +0530

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name="sakshitha",
+                DateOfBirth=new DateTime(2004,07,22)
+            };
+            return View(data);
+        }
+    }
+}
